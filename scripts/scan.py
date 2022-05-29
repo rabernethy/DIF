d155a66dbfb915fc896260a9e79060141c6da34e
Russell Abernethy <rdaswimmer@gmail.com>
2022-05-29 16:22:42 -0400

    added gps functionality

--- scripts/scan.py
+++ scripts/scan.py
@@ -1,6 +1,10 @@
 # scan.py written by Russell Abernethy
+from socket import timeout
+from serial import Serial
+from pynmeagps import NMEAReader
 from bluepy.btle import Scanner, DefaultDelegate
 import datetime
+
 
 class ScanDelegate(DefaultDelegate):
 	def __init__(self):
@@ -13,17 +17,31 @@ class ScanDelegate(DefaultDelegate):
 			print("Received New Data From", dev.addr)
 
 		
-def write_device_data_to_log():
+def write_device_data_to_log(nmr):
 	with open("logs/{}".format(datetime.datetime.now()),"w") as f:
+		# get a gps reading that has latitude, longitude, and time
+		found = False
+		while not found:
+			try:
+				(raw, parsed) = nmr.read()
+				lat = parsed.lat
+				lon = parsed.lon
+			except AttributeError:
+				continue
+		f.write("{lat}\n{lon}\n".format(lat=lat, lon=lon))
+
+
 		for dev in devices:
 			f.write("{addr}\n".format(addr=dev.addr))
 
 
 
 scanner = Scanner().withDelegate(ScanDelegate())
+stream = Serial('/dev/serial0', 9600, timeout=3)
+nmr = NMEAReader(stream)
 print("BLE Tote Scanner Started")
 while True:	
 	print("Scanning")
 	devices = scanner.scan(10.0)
 	print("Logging Scanned Devices")
-	write_device_data_to_log()
+	write_device_data_to_log(nmr)
